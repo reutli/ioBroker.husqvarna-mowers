eb00a872c922c0e199b4ebdb4b4bde47da8eae3a
micha <post@reutli.de>
2020-04-08 10:47:13 +0200

    alle Dateien comitten

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'husqvarna_mowers adapter settings': {
+		'en': 'Adapter settings for husqvarna_mowers',
+		'de': 'Adaptereinstellungen für husqvarna_mowers',
+		'ru': 'Настройки адаптера для husqvarna_mowers',
+		'pt': 'Configurações do adaptador para husqvarna_mowers',
+		'nl': 'Adapterinstellingen voor husqvarna_mowers',
+		'fr': "Paramètres d'adaptateur pour husqvarna_mowers",
+		'it': "Impostazioni dell'adattatore per husqvarna_mowers",
+		'es': 'Ajustes del adaptador para husqvarna_mowers',
+		'pl': 'Ustawienia adaptera dla husqvarna_mowers',
+		'zh-cn': 'husqvarna_mowers的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
